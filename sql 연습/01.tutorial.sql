c08704fd0c3ed3a910f1e7e9a9b087527fb19a36
kimjuhan-95/java-study <kimjuhan-95/java-study@gmail.com>
2025-08-13 12:00:47 +0900

    practices

--- sql 연습/01.tutorial.sql
+++ sql 연습/01.tutorial.sql
@@ -1,4 +1,23 @@
--- 버전 확인
-select version();
+-- 함수, 상수, 리터럴, 연산식
+select version(), current_date "hello", 1 +2 from dual;
 
+-- 수학함수, 문자열 함수, 날짜함수
+select sin(pi()/4), upper("seoul"), curdate() from dual;
+
+-- 대소문자 구문이 없다.
+select VERSION(), current_DATE From DuaL;
+
+-- table 생성: DDL
+create table pet (
+	name varchar(100),
+    owner varchar(50),
+    species varchar(50),
+    gender char(1),
+    birth date,
+    death date
+);
+
+-- schema  확인
+describe pet;
+desc pet;
 
